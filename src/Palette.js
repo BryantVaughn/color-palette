32b2627eb6d758f3fe5e41a2adb03df25ab135c1
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-03-23 17:24:48 -0600

    add and style Navbar component

--- src/Palette.js
+++ src/Palette.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
+import Navbar from './Navbar';
 import './Palette.css';
 
 class Palette extends Component {
@@ -30,28 +29,10 @@ class Palette extends Component {
 
     return (
       <div className="Palette">
-        <div className="slider">
-          <Slider
-            defaultValue={ level }
-            min={ 100 }
-            max={ 900 }
-            step={ 100 }
-            onAfterChange={ this.changeLevel }
-            trackStyle={{ backgroundColor: "transparent" }}
-            handleStyle={{
-              backgroundColor: "green",
-              outline: "none",
-              border: "2px solid green",
-              boxShadow: "none",
-              width: "13px",
-              height: "13px",
-              marginLeft: "-7px", 
-              marginTop: "-3px"
-            }}
-            railStyle={{ height: 8 }}
-          />
-        </div>
-        
+        <Navbar
+          level={ level }
+          changeLevel={ this.changeLevel }
+        />
         <div className="Palette-colors">
           { colorBoxes }
         </div>

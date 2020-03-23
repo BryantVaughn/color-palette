1e70c0a06c945edd1700c2691d862b79ff884f07
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-03-23 13:46:44 -0600

    Add ColorBox component

---
+++ src/ColorBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ColorBox.css';
+
+export default function ColorBox(props) {
+  return (
+    <div
+      style={{ background: props.background }}
+      className="ColorBox"
+    >
+      <span>{ props.name }</span>
+      <span>MORE</span>
+    </div>
+  );
+}
--- src/Palette.js
+++ src/Palette.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import ColorBox from './ColorBox';
+import './Palette.css';
 
-export default function Palette() {
+export default function Palette(props) {
+  const colorBoxes = props.colors.map(color => (
+    <ColorBox
+      background={ color.color }
+      name={ color.name }
+      id={ color.id }
+    />
+  ));
+
   return (
     <div className="Palette">
       <div className="Palette-colors">
-        
+        { colorBoxes }
       </div>
     </div>
   );

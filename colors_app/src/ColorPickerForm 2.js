21b5caa8c820668908d7d2341d356d7249fd0815
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-04-07 10:59:38 -0600

    Final clean up

---
+++ colors_app/src/ColorPickerForm 2.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import { ChromePicker } from 'react-color';
+import styles from './styles/ColorPickerFormStyles';
+
+class ColorPickerForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentColor: "teal",
+      newColorName: ""
+    };
+  }
+
+  componentDidMount = () => {
+    ValidatorForm.addValidationRule("isColorNameUnique", value => 
+      this.props.colors.every(
+        ({ name }) => name.toLowerCase() !== value.toLowerCase()
+      )
+    );
+    ValidatorForm.addValidationRule("isColorUnique", value => 
+      this.props.colors.every(
+        ({ color }) => color !== this.state.currentColor
+      )
+    );
+  };
+
+  updateCurrentColor = (newColor) => {
+    this.setState({ currentColor: newColor.hex });
+  };
+
+  handleChange = (evt) => {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  };
+
+  handleSubmit = () => {
+    const newColor = {
+      color: this.state.currentColor,
+      name: this.state.newColorName
+    };
+    this.props.addNewColor(newColor);
+    this.setState({ newColorName: "" });
+  };
+
+  render() {
+    const { paletteIsFull, classes } = this.props;
+    const { currentColor, newColorName } = this.state;
+
+    return (
+      <div>
+        <ChromePicker
+            color={ currentColor }
+            onChangeComplete={ this.updateCurrentColor }
+            className={ classes.picker }
+          />
+          <ValidatorForm onSubmit={ this.handleSubmit } instantValidate={ false }>
+            <TextValidator
+              className={ classes.colorNameInput }
+              placeholder="Color Name"
+              value={ newColorName }
+              name="newColorName"
+              variant="filled"
+              margin="normal"
+              onChange={ this.handleChange }
+              validators={[
+                "required",
+                "isColorNameUnique",
+                "isColorUnique"
+              ]}
+              errorMessages={[
+                "color name is required",
+                "color name must be unique",
+                "color already used"
+              ]}
+            />
+            <Button
+            className={ classes.addColor }
+              variant="contained"
+              type="submit"
+              color="primary"
+              disabled={ paletteIsFull }
+              style={{
+                backgroundColor: paletteIsFull
+                ? "rgba(0,0,0,0.12)"
+                : currentColor
+              }}
+            >
+              { paletteIsFull ? "Palette Full" : "Add Color" }
+            </Button>
+          </ValidatorForm>
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(ColorPickerForm);
---
+++ colors_app/src/PaletteFormNav 2.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
+import classNames from 'classnames';
+import PaletteMetaForm from './PaletteMetaForm';
+import styles from './styles/PaletteFormNavStyles';
+
+class PaletteFormNav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newPaletteName: "",
+      formShowing: false
+    }
+  }
+
+  handleChange = (evt) => {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  };
+
+  showForm = () => {
+    this.setState({ formShowing: true });
+  };
+
+  hideForm = () => {
+    this.setState({ formShowing: false });
+  };
+
+  render() {
+    const { classes, open, palettes, handleSubmit, handleDrawerOpen } = this.props;
+    const { formShowing } = this.state;
+
+    return (
+      <div className={ classes.root }>
+        <AppBar
+          postion="fixed"
+          color="default"
+          className={ classNames(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar disableGutters={ !open }>
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              onClick={ handleDrawerOpen }
+              className={ classNames(classes.menuButton, {
+                [classes.hide]: open
+              })}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" noWrap>
+              New Color Palette
+            </Typography>
+          </Toolbar>
+          <div className={ classes.navBtns }>
+            <Link to="/">
+              <Button
+                className={ classes.button }
+                variant="contained"
+                color="secondary"
+              >
+                Go Back
+              </Button>
+            </Link>
+            <Button
+              className={ classes.button }
+              variant="contained"
+              color="primary"
+              onClick={ this.showForm }
+            >
+              Save
+            </Button>
+          </div>
+        </AppBar>
+        { formShowing && (
+          <PaletteMetaForm
+            palettes={ palettes }
+            handleSubmit={ handleSubmit }
+            hideForm={ this.hideForm }
+          /> 
+        )}
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles, { withTheme: true })(PaletteFormNav);
